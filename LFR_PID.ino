119066ce9b56779299fb256339b1e6bec84a37eb
Md. Arif Hossain <arifhossain19810@gmail.com>
2026-09-21 23:30:12 +0000

    pid 1st code

--- LFR_PID.ino
+++ LFR_PID.ino
@@ -11,9 +11,11 @@
 #define espResetPin 31
 
 const int NUM_SENSORS = 13;
-int sensorPins[NUM_SENSORS] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+const int sensorPins[NUM_SENSORS] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
-int threshold[NUM_SENSORS] = {150, 255, 225, 150, 200, 250, 250, 250, 150, 150, 215, 150, 250};
+int threshold[NUM_SENSORS] = {
+  150, 255, 225, 150, 200, 250, 250, 250, 150, 150, 215, 150, 250
+};
 
 float Kp = 0.15;
 float Ki = 0.0002;
@@ -25,11 +27,53 @@ float Kd_sharp = 1.2;
 int baseSpeed = 180;
 int maxSpeed = 255;
 
-int sensorWeights[NUM_SENSORS] = {-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6};
+const int sensorWeights[NUM_SENSORS] = {
+  -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6
+};
 
-long lastError = 0;
-long integral = 0;
-unsigned long lastTime = 0;
+struct PIDController {
+  float Kp, Ki, Kd;
+  float integral = 0;
+  float lastError = 0;
+  unsigned long lastTime = 0;
+  float integralLimit = 10000;
+
+  PIDController(float p, float i, float d) : Kp(p), Ki(i), Kd(d) {}
+
+  void reset() {
+    integral = 0;
+    lastError = 0;
+    lastTime = millis();
+  }
+
+  void setGains(float p, float i, float d) {
+    Kp = p; Ki = i; Kd = d;
+  }
+
+  float compute(float error) {
+    unsigned long now = millis();
+    float dt = (now - lastTime) / 1000.0f;
+    lastTime = now;
+
+    if (dt <= 0) dt = 0.001f;
+
+    integral += error * dt;
+    integral = constrain(integral, -integralLimit, integralLimit);
+
+    float derivative = (error - lastError) / dt;
+    lastError = error;
+
+    return Kp * error + Ki * integral + Kd * derivative;
+  }
+};
+
+PIDController pid(Kp, Ki, Kd);
+
+enum State { NORMAL, ZIGZAG, JUNCTION, GAP };
+State currentState = NORMAL;
+
+uint16_t sensorBits = 0;
+int activeCount = 0;
 
 bool zigzagMode = false;
 unsigned long lastSignChange = 0;
@@ -38,11 +82,7 @@ bool wasLineLost = false;
 unsigned long lineLostTime = 0;
 const unsigned long GAP_TIMEOUT = 150;
 
-enum State { NORMAL, ZIGZAG, JUNCTION, GAP };
-State currentState = NORMAL;
-
-uint16_t sensorBits = 0;
-int activeCount = 0;
+int turnCount = 0;
 
 void setup() {
   bitSet(ADCSRA, ADPS2);
@@ -65,7 +105,8 @@ void setup() {
   digitalWrite(espResetPin, HIGH);
 
   Serial.begin(115200);
-  lastTime = millis();
+  
+  pid.lastTime = millis();
 }
 
 void loop() {
@@ -77,7 +118,7 @@ void loop() {
 void readSensors() {
   sensorBits = 0;
   activeCount = 0;
-  
+
   for (int i = 0; i < NUM_SENSORS; i++) {
     int raw = analogRead(sensorPins[i]);
     bool onLine = (raw > threshold[i]);
@@ -88,11 +129,28 @@ void readSensors() {
   }
 }
 
-void updateState() {
-  int pos = getLinePosition();
-  long error = (pos >= 0) ? (pos - 5000) : lastError;
+int computeLinePosition() {
+  long weightedSum = 0;
+  int activeSensors = 0;
 
-  if (pos == -1) {
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    if (sensorBits & (1 << (NUM_SENSORS - 1 - i))) {
+      weightedSum += sensorWeights[i];
+      activeSensors++;
+    }
+  }
+
+  if (activeSensors == 0) return 0;
+
+  float position = (float)weightedSum / activeSensors;
+  return (int)(position * 100.0f / 6.0f);
+}
+
+void updateState() {
+  int position = computeLinePosition();
+  long error = (activeCount > 0) ? position : pid.lastError;
+
+  if (activeCount == 0) {
     if (!wasLineLost) {
       lineLostTime = millis();
       wasLineLost = true;
@@ -107,7 +165,7 @@ void updateState() {
     return;
   }
 
-  if ((error > 800 && lastError < -800) || (error < -800 && lastError > 800)) {
+  if ((error > 80 && pid.lastError < -80) || (error < -80 && pid.lastError > 80)) {
     if (millis() - lastSignChange < 400) {
       zigzagMode = true;
       currentState = ZIGZAG;
@@ -123,54 +181,22 @@ void updateState() {
   }
 }
 
-int getLinePosition() {
-  long weightedSum = 0;
-  int sum = 0;
-
-  for (int i = 0; i < NUM_SENSORS; i++) {
-    if (sensorBits & (1 << (NUM_SENSORS - 1 - i))) {
-      weightedSum += sensorWeights[i] * 1000;
-      sum++;
-    }
-  }
-
-  if (sum == 0) return -1;
-
-  return (int)(weightedSum / sum) + 5000;
-}
-
 void runPID() {
-  unsigned long now = millis();
-  float dt = (now - lastTime) / 1000.0;
-  lastTime = now;
-
-  int pos = getLinePosition();
-  long error;
-
-  if (pos == -1) {
-    error = lastError;
-  } else {
-    error = pos - 5000;
-    if (currentState != GAP) {
-      integral += error * dt;
-      integral = constrain(integral, -10000, 10000);
-    }
-  }
-
-  long derivative = (error - lastError) / dt;
+  int position = computeLinePosition();
+  long error = (activeCount > 0) ? position : pid.lastError;
 
   float Kp_use = zigzagMode ? Kp_sharp : Kp;
   float Kd_use = zigzagMode ? Kd_sharp : Kd;
   int speed_use = zigzagMode ? baseSpeed * 0.7 : baseSpeed;
 
-  float output = Kp_use * error + Ki * integral + Kd_use * derivative;
+  pid.setGains(Kp_use, Ki, Kd_use);
 
-  int leftSpeed = constrain(speed_use + output, -maxSpeed, maxSpeed);
-  int rightSpeed = constrain(speed_use - output, -maxSpeed, maxSpeed);
+  float correction = pid.compute(error);
+
+  int leftSpeed = constrain(speed_use + correction, -maxSpeed, maxSpeed);
+  int rightSpeed = constrain(speed_use - correction, -maxSpeed, maxSpeed);
 
   setMotors(leftSpeed, rightSpeed);
-
-  lastError = error;
 
   if (currentState == JUNCTION) {
     handleJunction();
@@ -180,26 +206,23 @@ void runPID() {
 void handleJunction() {
   setMotors(0, 0);
   delay(100);
-  
+
   int turnDir = (turnCount % 2 == 0) ? 1 : -1;
   turnCount++;
-  
+
   unsigned long start = millis();
   while (millis() - start < 500) {
     readSensors();
-    if (activeCount < 5 && getLinePosition() != -1) break;
+    if (activeCount < 5 && computeLinePosition() != 0) break;
     setMotors(turnDir * 200, -turnDir * 200);
     delay(10);
   }
-  
-  integral = 0;
-  lastError = 0;
+
+  pid.reset();
 }
 
-int turnCount = 0;
-
 void setMotors(int left, int right) {
-  if (left > 0) {
+  if (left >= 0) {
     digitalWrite(L1, HIGH);
     digitalWrite(L2, LOW);
   } else {
@@ -208,7 +231,7 @@ void setMotors(int left, int right) {
     left = -left;
   }
 
-  if (right > 0) {
+  if (right >= 0) {
     digitalWrite(R1, HIGH);
     digitalWrite(R2, LOW);
   } else {
@@ -222,4 +245,30 @@ void setMotors(int left, int right) {
 
   analogWrite(enaL, left);
   analogWrite(enaR, right);
+}
+
+void calibrateSensors() {
+  int maxi[NUM_SENSORS];
+  int mini[NUM_SENSORS];
+
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    maxi[i] = 0;
+    mini[i] = 1023;
+  }
+
+  setMotors(80, -80);
+  for (int j = 0; j < 5000; j++) {
+    for (int i = 0; i < NUM_SENSORS; i++) {
+      int val = analogRead(sensorPins[i]);
+      maxi[i] = max(maxi[i], val);
+      mini[i] = min(mini[i], val);
+    }
+  }
+  setMotors(0, 0);
+
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    threshold[i] = (maxi[i] - mini[i]) * 0.3 + mini[i];
+    EEPROM.write(i, threshold[i] / 4);
+    delay(10);
+  }
 }
